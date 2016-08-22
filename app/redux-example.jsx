ed35f10c564b8f6d533ccf6d40ab3649e2b09e1b
pentel13 <bmorelli25@gmail.com>
2016-08-22 10:24:23 -0700

    Add Reducer and start Redux
    no actions in reducer yet

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,13 @@
 var redux = require('redux');
 
 console.log('Starting Redux Example');
+
+var reducer = (state = {name: 'Anonymous'}, action) => {
+  //state = state || {name: 'Anonymous'}; // default state
+
+  return state;
+};
+var store = redux.createStore(reducer); // reducer must be a pure function
+
+var currentState = store.getState();
+console.log('currentState: ', currentState);
