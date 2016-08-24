b27862943947e470c8c8efda4fba4631d49e6efa
pentel13 <bmorelli25@gmail.com>
2016-08-24 13:37:15 -0700

    Redux Actions: Removing from Arrays
    Removing from arrays in redux via filter

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -30,6 +30,11 @@ var reducer = (state = stateDefault, action) => {
           }
         ]
       };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
     case 'ADD_MOVIE':
       return {
         ...state,
@@ -42,9 +47,14 @@ var reducer = (state = stateDefault, action) => {
           }
         ]
       };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      };
     default:
       return state;
-  }
+  };
 };
 
 var store = redux.createStore(reducer, redux.compose(
@@ -76,6 +86,16 @@ store.dispatch({
 });
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
   type: 'ADD_MOVIE',
   title: 'Inside Man',
   genre: 'Action'
@@ -90,4 +110,9 @@ store.dispatch({
   type: 'ADD_MOVIE',
   title: 'Titanic',
   genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
